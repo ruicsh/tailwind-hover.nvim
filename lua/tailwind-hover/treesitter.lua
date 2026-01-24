3517495bfd26284b7aca92076dd4bae5840a8abd
ruicsh <ruicosta042@gmail.com>
2026-01-24 00:06:53 +0000

    feat: only parse if cursor is on a class/className attribute's value

--- lua/tailwind-hover/treesitter.lua
+++ lua/tailwind-hover/treesitter.lua
@@ -1,104 +1,5 @@
 local utils = require("tailwind-hover.utils")
 
-local standard = function(lang)
-	return vim.treesitter.query.parse(
-		lang,
-		[[
-    (attribute
-      (attribute_name) @attr_name
-        (quoted_attribute_value (attribute_value) @values)
-        (#match? @attr_name "class")
-    )
-    ]]
-	)
-end
-
-local tsx_parser = function()
-	return {
-		vim.treesitter.query.parse(
-			"tsx",
-			[[
-            (jsx_attribute
-              (property_identifier) @attr_name
-              (#match? @attr_name "className")
-              (string
-              (string_fragment) @values
-              )
-            )
-        ]]
-		),
-		vim.treesitter.query.parse(
-			"tsx",
-			[[
-              (string
-              (string_fragment) @values
-              )
-        ]]
-		),
-	}
-end
-
--- For template strings
-local typescript_parser = function()
-	return {
-		standard("html"),
-	}
-end
-
-local astro_parser = function()
-	return {
-		standard("astro"),
-	}
-end
-
-local vue_parser = function()
-	return {
-		standard("vue"),
-	}
-end
-
-local svelte_parser = function()
-	return {
-		standard("svelte"),
-	}
-end
-
-local html_parser = function()
-	return {
-		standard("html"),
-	}
-end
-
-local templ_parser = function()
-	return {
-		standard("templ"),
-	}
-end
-
-local parsers = {
-	typescriptreact = tsx_parser,
-	typescript = typescript_parser,
-	astro = astro_parser,
-	vue = vue_parser,
-	svelte = svelte_parser,
-	html = html_parser,
-	templ = templ_parser,
-}
-
-local function get_treesitter(bufnr)
-	bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-	local ft = vim.bo[bufnr].ft
-
-	if parsers[ft] == nil then
-		return {
-			standard("html"),
-		}
-	end
-
-	return parsers[ft]()
-end
-
 -- Get the tailwindcss LSP client
 local function get_tw_client()
 	local clients = vim.lsp.get_clients({ name = "tailwindcss" })
@@ -118,40 +19,36 @@ M.get_tw_classes_at_cursor = function(bufnr)
 	local node = vim.treesitter.get_node({ bufnr = bufnr, ignore_injections = false })
 	if node == nil then
 		print("No node found")
-		return
+		return {}
 	end
 
-	local parent = node:parent()
-	if not parent then
-		print("No parent found")
-		return
+	local type = node:type()
+	if type ~= "string_fragment" and type ~= "attribute_value" then
+		return {}
 	end
 
-	local queries = get_treesitter(bufnr)
-	if queries == nil then
-		print("No parser found")
-		return
+	-- Get the attribute node
+	local attribute = node:parent():parent()
+	if attribute == nil then
+		return {}
 	end
 
-	local found_match = false
-	for _, query in ipairs(queries) do
-		if found_match then
-			break
-		end
-		for id, node in query:iter_captures(parent, bufnr, 0, -1) do
-			found_match = true
-			local name = query.captures[id]
-
-			if name == "values" then
-				local values = vim.treesitter.get_node_text(node, bufnr)
-				local range = { vim.treesitter.get_node_range(node) }
-
-				return values, range
-			end
-		end
+	-- Check if the parent is an attribute or jsx_attribute
+	local attribute_type = attribute:type()
+	if attribute_type ~= "attribute" and attribute_type ~= "jsx_attribute" then
+		return {}
 	end
 
-	return {}
+	-- Check if the attribute is class or className
+	local attribute_text = vim.treesitter.get_node_text(attribute, bufnr)
+	if not (vim.startswith(attribute_text, "class=") or vim.startswith(attribute_text, "className=")) then
+		return {}
+	end
+
+	local values = vim.treesitter.get_node_text(node, bufnr)
+	local range = { vim.treesitter.get_node_range(node) }
+
+	return values, range
 end
 
 M.parse_with_tailwind = function(params, cb)
